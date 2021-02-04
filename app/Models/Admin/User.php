5ea6ef0ad9dae9c439ab108e4d5ae137782b861e
edwardkv <edwardkv@gmail.com>
2021-02-04 10:02:20 +0400

    create auth step 1

---
+++ app/Models/Admin/User.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models\Admin;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Model
+{
+    use HasFactory;
+
+    use SoftDeletes;
+    use Notifiable;
+
+    protected $fillable = [
+        'id',
+        'name',
+        'login',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    /**
+     * Роли, принадлежащие пользователю.
+     */
+    public function roles()
+    {
+        return $this->belongsToMany('App\Models\Role','user_roles');
+    }
+
+}
