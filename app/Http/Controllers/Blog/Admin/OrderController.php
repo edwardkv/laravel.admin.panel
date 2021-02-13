1f0b8a82193fa33e67ce8e4ee405385966cb25fe
edwardkv <edwardkv@gmail.com>
2021-02-13 14:40:08 +0400

    delete orders

--- app/Http/Controllers/Blog/Admin/OrderController.php
+++ app/Http/Controllers/Blog/Admin/OrderController.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Http\Controllers\Blog\Admin;
-
+use App\Http\Requests\AdminOrderSaveRequest;
+use App\Models\Admin\Order;
 use App\Repositories\Admin\MainRepository;
 use App\Repositories\Admin\OrderRepository;
 use Illuminate\Http\Request;
@@ -108,9 +109,35 @@ class OrderController extends AdminBaseController
 
     }
 
+    public function save(AdminOrderSaveRequest $request, $id)
+    {
+        $result = $this->orderRepository->saveOrderComment($id);
+        if ($result) {
+            return redirect()
+                ->route('blog.admin.orders.edit', $id)
+                ->with(['success' => 'Успешно сохранено']);
+        } else {
+            return back()
+                ->withErrors(['msg' => "Ошибка сохранения"]);
+        }
+    }
+
     public function forcedestroy($id)
     {
+        if (empty($id)){
+            return back()->withErrors(['msg' => 'Запись не найдена']);
+        }
 
+        $res = \DB::table('orders')
+            ->delete($id);
+
+        if ($res) {
+            return redirect()
+                ->route('blog.admin.orders.index')
+                ->with(['success' => "Запись id [$id] удалена из БД"]);
+        } else {
+            return back()->withErrors(['msg' => 'Ошибка удаления']);
+        }
     }
 
     /**
@@ -133,6 +160,18 @@ class OrderController extends AdminBaseController
      */
     public function destroy($id)
     {
-        //
+        $st = $this->orderRepository->changeStatusOnDelete($id);
+        if ($st) {
+            $result = Order::destroy($id);
+            if ($result) {
+                return redirect()
+                    ->route('blog.admin.orders.index')
+                    ->with(['success' => "Запись id [$id] удалена"]);
+            } else {
+                return back()->withErrors(['msg' => 'Ошибка удаления']);
+            }
+        } else {
+            return back()->withErrors(['msg' => 'Статут не изменился']);
+        }
     }
 }
